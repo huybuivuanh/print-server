4225fbcb0d42bc79e3624f0451bf83bdeaf14303
huybuivuanh <huy_buivu12345@yahoo.ca>
2026-05-28 21:15:47 -0600

    demo database code

--- firestore.js
+++ firestore.js
@@ -8,3 +8,15 @@ admin.initializeApp({
 const db = admin.firestore();
 
 module.exports = { db };
+
+// const admin = require("firebase-admin");
+// const { getFirestore } = require("firebase-admin/firestore");
+// const serviceAccount = require("./admin-sdk.json");
+
+// const app = admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// const db = getFirestore(app, "demo");
+
+// module.exports = { db };
